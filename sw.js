0ae789c688a5127d2a27de948069d01fe4fedf27
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2026-06-07 22:06:27 +0900

    feat: add PWA support with offline capability

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'brew-calc-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/images/logo.png'
+];
+
+// インストール時にリソースをキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 古いキャッシュの削除
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// リクエストに対してキャッシュがあればそれを返し、なければネットワークから取得
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
